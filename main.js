3154c8f4349130d4e66f85a9847efc514dff575c
Cederlund1999 <Cederlund1999@gmail.com>
2021-02-10 16:22:45 +0100

    Lite uppdatering med foursquare call monument

--- main.js
+++ main.js
@@ -9,7 +9,20 @@ const fourApi = {
 };
 
 
-
+/*function getCityWeatherUrl(city) {
+  let url = new URL("https://api.openweathermap.org/data/2.5/weather");
+  url.searchParams.append("q", city);
+  console.log(url+" här är url");
+  console.log(city +"citynamnhehe");
+  url.searchParams.append("appid", "d302c80ce01e6c66f271e7216cd41e93");
+  url.searchParams.append("units", "metric");
+  return url;
+  
+}
+let city = "London";
+let url = getCityWeatherUrl(city);
+window.url = url;
+console.log(url);*/
 
 
 
@@ -37,15 +50,24 @@ function displayWeatherResults(response){
     temperature.innerHTML = (response.main.temp);
     let city = document.querySelector('.location .city');
     city.innerHTML = (response.name);
+    let currentWeather = document.querySelector('.current .weather');
+    currentWeather.innerHTML = (response.weather[0].main);
 }
 
 function getPlacesResults(q){
-    fetch(`${fourApi.base}?client_id=${fourApi.CLIENT_ID}&client_secret=${fourApi.CLIENT_SECRET}&near=${q}&query=sushi&v=20210204`).then(venues => {
+    fetch(`${fourApi.base}?client_id=${fourApi.CLIENT_ID}&client_secret=${fourApi.CLIENT_SECRET}&near=${q}&categoryId=4bf58dd8d48988d12d941735&limit=4&v=20210204`).then(venues => {
         return venues.json()}).then(displayPlacesResults);
         
 }
 function displayPlacesResults(venues){
 console.log(venues);
+let firstMonument = document.querySelector('.top3 .place1');
+firstMonument.innerHTML = (venues.response.venues[0].name + " <br> Adress:  " + venues.response.venues[0].location.address); 
+let secondMonument = document.querySelector('.top3 .place2');
+secondMonument.innerHTML = (venues.response.venues[1].name + " <br> Adress:  " + venues.response.venues[1].location.address);
+let thirdMonument = document.querySelector('.top3 .place3');
+thirdMonument.innerHTML = (venues.response.venues[2].name + " <br> Adress:  " + venues.response.venues[2].location.address);
+
 
 }
 
