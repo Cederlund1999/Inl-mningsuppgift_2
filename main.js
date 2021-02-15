dfd92b544da5772eb01474c0fec17ef316c9f90d
Cederlund1999 <cederlund1999@gmail.com>
2021-02-15 16:01:19 +0100

    Kommentarer, datum m.m

--- main.js
+++ main.js
@@ -8,21 +8,6 @@ const fourApi = {
   base: "https://api.foursquare.com/v2/venues/search",
 };
 
-/*function getCityWeatherUrl(city) {
-  let url = new URL("https://api.openweathermap.org/data/2.5/weather");
-  url.searchParams.append("q", city);
-  console.log(url+" här är url");
-  console.log(city +"citynamnhehe");
-  url.searchParams.append("appid", "d302c80ce01e6c66f271e7216cd41e93");
-  url.searchParams.append("units", "metric");
-  return url;
-  
-}
-let city = "London";
-let url = getCityWeatherUrl(city);
-window.url = url;
-console.log(url);*/
-
 const searchInput = document.querySelector(".searchbox");
 searchInput.addEventListener("keypress", executeSearch);
 
@@ -32,13 +17,23 @@ function executeSearch(s) {
     getPlacesResults(searchInput.value);
   }
 }
-
+/* Här använder jag fetch i en funktion för att göra en request till den url'en jag vill och får tillbaks ett respons objekt. 
+Sedan om responsen är ok görs den om till json för att jag ska kunna läsa av och använda responsen jag fick från servern.
+Sedan körs min funktion displayWeatherResults där jag använder responsen jag fått för att uppdatera i DOM och visa vilken temperatur det är etc.
+Varje gång en användare skriver något i sökrutan och trycker på "enter" så körs denna funktionen och gör en request. */
 function getWeatherResults(q) {
   fetch(`${weatherApi.base}weather?q=${q}&units=metric&appid=${weatherApi.key}`)
     .then((response) => {
-      return response.json();
+      if (response.ok) {
+        console.log(response);
+        return response.json();
+      } else {
+        alert("City not found, try again!");
+      }
     })
+
     .then(displayWeatherResults);
+
   console.log(searchInput.value);
 }
 
@@ -55,7 +50,7 @@ function displayWeatherResults(response) {
     document.getElementById("weatherPicture").src = iconURL;
 
     let temperature = document.querySelector(".current .temperature");
-    temperature.innerHTML = response.main.temp +"°C";
+    temperature.innerHTML = Math.round(response.main.temp) + "°C";
     let city = document.querySelector(".location .city");
     city.innerHTML = response.name + ", " + response.sys.country;
     let currentWeather = document.querySelector(".current .weather");
@@ -65,13 +60,22 @@ function displayWeatherResults(response) {
     document.getElementById("currentLocation").style.display = "none";
   }
 }
-
+/*
+Här händer det exakt samma sak som föregående fast med foursquare api'n istället. 
+I url'en skickar jag med vad jag vill få för respons. Jag skickar med att jag vill ha resultat i närheten av sökinputen, och skickad med kategori id vilket i detta fall är monument.
+Jag vill ha ett svar med max 4 monument.
+Jag anger att jag är redo för API ändringar fram tills datumet jag angett i "v"
+*/
 function getPlacesResults(q) {
   fetch(
-    `${fourApi.base}?client_id=${fourApi.CLIENT_ID}&client_secret=${fourApi.CLIENT_SECRET}&near=${q}&categoryId=4bf58dd8d48988d12d941735&limit=4&v=20210204`
+    `${fourApi.base}?client_id=${fourApi.CLIENT_ID}&client_secret=${fourApi.CLIENT_SECRET}&near=${q}&categoryId=4bf58dd8d48988d12d941735&limit=4&v=20210215`
   )
     .then((venues) => {
-      return venues.json();
+      if (venues.ok) {
+        return venues.json();
+      } else {
+        console.log("no monuments available for this city");
+      }
     })
     .then(displayPlacesResults);
 }
@@ -93,11 +97,18 @@ function displayPlacesResults(venues) {
       " <br> Adress:  " +
       venues.response.venues[1].location.address;
     let thirdMonument = document.querySelector(".top3 .place3");
-    thirdMonument.innerHTML = 
+    thirdMonument.innerHTML =
       venues.response.venues[2].name +
       " <br> Adress:  " +
       venues.response.venues[2].location.address;
   } else {
     document.getElementById("top3Venues").style.display = "none";
   }
+}
+{
+  let newDate = new Date();
+  let year = newDate.getFullYear();
+  let month = newDate.getMonth() + 1;
+  let date = newDate.getDate();
+  document.getElementById("date").innerHTML = month + "/" + date + "/" + year;
 }
